aa0a9e5ffc519ce5cc3fd6ebe76ebb8f681fa3d9
1412350 <ngoc120742@gmail.com>
2017-10-14 18:05:19 +0700

    Add confirm account of user

---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings ={
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'thegearsweb@gmail.com',
+  password:             'ngoc120742',
+  authentication:       'plain',
+  enable_starttls_auto: true  
+}
