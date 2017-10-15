afdab1f058db54f27bfc7861803fc439157bec97
1412350 <ngoc120742@gmail.com>
2017-10-15 20:52:03 +0700

    Generate conversation & message model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :name, presence: true
+  has_many :messages
+  has_many :conversations, foreign_key: :sender_id
 end
