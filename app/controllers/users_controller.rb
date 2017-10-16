3b78017e9ea07600e9567987bf22f96d7ecfe199
1412350 <ngoc120742@gmail.com>
2017-10-17 00:52:33 +0700

    update chat action 2 realtime chat between 2 user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,7 @@ class UsersController < ApplicationController
     @users = User.all.where.not(id: current_user)
     @conversations = Conversation.includes(:recipient, :messages).find(session[:conversations])
   end
+  def get_conversation(user_id)
+    return Conversation.between(current_user.id, user_id).first
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :name, presence: true
   has_many :messages
-  has_many :conversations, foreign_key: :sender_id
+  has_many :conversations, foreign_key: :sender_id 
+  def get_conversation(user_id1, user_id2)
+    conversation = Conversation.between(user_id1, user_id2).first
+  end
+  def get_conversation_last_message(user_id1, user_id2)
+    conversation = Conversation.between(user_id1, user_id2).first
+    if conversation.present?
+      if conversation.messages.count >= 1
+        return conversation.messages.last.content  
+      else return 'Has no message yet'
+      end
+    else
+      if conversation.messages.count >= 1
+        return conversation.messages.last.content  
+      else return 'Has no message yet'
+      end
+    end  
+  end
 end
