9c1942a02175640e9e8b799d553af59c4aaf640d
1412350 <ngoc120742@gmail.com>
2017-10-16 17:39:04 +0700

    Add create a conversation and chat action

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,21 @@
 //= require popper
 //= require bootstrap
 //= require_tree .
+(function() {
+  $(document).on('click', '.toggle-window', function(e) {
+    e.preventDefault();
+    var panel = $(this).parent().parent();
+    var messages_list = panel.find('.messages-list');
+ 
+    panel.find('.card-body').toggle();
+    panel.find('.card-footer').toggle();
+    panel.attr('class', 'card');
+ 
+    if (panel.find('.card-body').is(':visible')) {
+      var height = messages_list[0].scrollHeight;
+      messages_list.scrollTop(height);
+    }
+    
+  });
+})();
+
