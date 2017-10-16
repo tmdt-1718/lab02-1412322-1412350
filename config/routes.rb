0f651a2fcd597045b048e2a45d24e6a28fa17982
1412350 <ngoc120742@gmail.com>
2017-10-16 17:39:04 +0700

    Add create a conversation and chat action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
+    resources :messages, only: [:create]
+  end
 end
