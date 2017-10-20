cf2dc37ec2236f7d62c3dd921d14f6e0bcbc3db9
1412322 <nhatminh190496@gmail.com>
2017-10-20 12:33:17 +0700

    Fix FB login

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    omniauth_callbacks: "users/omniauth_callbacks"
+    :omniauth_callbacks => "users/omniauth_callbacks"
   }
   resources :conversations, only: [:create, :index, :show] do
     member do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019034031) do
+ActiveRecord::Schema.define(version: 20171020050755) do
 
   create_table "conversations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "recipient_id"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 20171019034031) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
